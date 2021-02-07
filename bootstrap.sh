087e233e9b1f763d2f12fc5fa61a911ad0105e04
Your Name <you@example.com>
2021-02-08 03:16:10 +0600

    now chromium is fully visible

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-Xvfb -ac :99 -screen 0 1280x720x16 &
+Xvfb -ac :99 -screen 0 1000x1000x16 &
 export DISPLAY=:99
-google-chrome --no-sandbox &
-x11vnc -nopw
+google-chrome --no-sandbox --window-position=0,0 --window-size=900,700 &
+x11vnc -nopw -forever
