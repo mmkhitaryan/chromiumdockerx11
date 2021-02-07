b98e948644db982fca32d11ca907924d93d98b7f
Your Name <you@example.com>
2021-02-08 03:24:41 +0600

    all done, now need to finnish dbus for push

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
+service dbus start
 Xvfb -ac :99 -screen 0 1000x1000x16 &
 export DISPLAY=:99
-google-chrome --no-sandbox --window-position=0,0 --window-size=900,700 &
+google-chrome --no-sandbox --window-position=0,0 --window-size=900,700 --disable-gpu &
 x11vnc -nopw -forever
