f89c11b03896bd664296ca99b8cfceaff625cfb6
Your Name <you@example.com>
2021-02-08 00:37:44 +0600

    oopsie, forgot to include

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@ x11vnc -create -env FD_PROG=/usr/bin/startxfce4 \
     -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 \
         -env X11VNC_CREATE_GEOM=${1:-1024x768x16} \
         -gone 'killall Xvfb' \
-        -nopw -forever
+        -nopw -forever &
+xvfb-run google-chrome --no-sandbox
