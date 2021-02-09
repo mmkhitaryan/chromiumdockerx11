4955184cc568d21397cd71d1cc3ebfcdbdcb57c9
Your Name <you@example.com>
2021-02-09 23:39:36 +0600

    now I can receive push text and title

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,11 @@ RUN apt-get install -y wget
 RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 RUN apt install -y ./google-chrome-stable_current_amd64.deb
 RUN rm google-chrome-stable_current_amd64.deb
-RUN apt-get install -y xfce4 dbus-x11 --no-install-recommends
+RUN apt-get install -y xfce4 dbus-x11 python3-pip --no-install-recommends
 RUN apt-get install -y x11vnc xvfb xfwm4
 RUN apt-get install -y xfce4-notifyd
 ADD bootstrap.sh /root/bootstrap.sh
 ADD listener.py /root/listener.py
+ADD requirements.txt /root/requirements.txt
+RUN python3 -m pip install -r /root/requirements.txt
 ENTRYPOINT ["/bin/bash", "/root/bootstrap.sh"]
--- listener.py
+++ listener.py
@@ -1,11 +1,22 @@
-# method call time=1612879746.349075 sender=:1.6 -> destination=org.freedesktop.Notifications serial=11 path=/org/freedesktop/Notifications; interface=org.freedesktop.Notifications; member=Notify
 import dbus
+import requests
 from gi.repository import GLib
+import threading
 
 from dbus.mainloop.glib import DBusGMainLoop
 
+def send_req(push_title, push_body):
+    requests.post('https://webhook.site/e7423247-ad55-49b6-bbef-3b054b4be8bb', data={'push_title':push_title,'push_body':push_body})
+
 def notifications(bus, message):
-    print([arg for arg in message.get_args_list()])
+    args = [str(arg) for arg in message.get_args_list()]
+
+    # dbus.String('Web Push Book'), dbus.String('<a href="https://web-push-book.gauntface.com/">web-push-book.gauntface.com</a>\n\nThis would be the body text of the notification.\nIt can hold two lines of text.'), dbus.Array([dbus.String('0'), dbus.String('Download Book')
+    push_title = args[3]
+    push_body = args[4]
+
+    threading.Thread(target=send_req, args=(push_title, push_body)).start()
+    print(args)
 
 DBusGMainLoop(set_as_default=True)
 
