687f3d37bd70503cad148c45b4ec1d3a3623a94f
Your Name <you@example.com>
2021-02-09 20:11:37 +0600

    dbus works

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 service dbus start
-export $(dbus-launch)
+x=$(dbus-launch)
+export $x
+echo $x | tr '\0' '\n'
 Xvfb -ac :99 -screen 0 1000x1000x16 &
 export DISPLAY=:99
-/usr/bin/startxfce4 &
 /usr/lib/x86_64-linux-gnu/xfce4/notifyd/xfce4-notifyd &
 google-chrome --no-sandbox --window-position=0,0 --window-size=900,700 --disable-gpu https://web-push-book.gauntface.com/demos/notification-examples/ &
 x11vnc -nopw -forever
